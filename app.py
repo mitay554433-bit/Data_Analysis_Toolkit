3d07d7e38b9f30c8e87dafd80d29bd4aa972a880
mitay554433-bit <mitay554433@gmail.com>
2026-04-15 11:07:05 -0700

    fix render port

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-
+import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+PORT = int(os.environ.get("PORT", 8080))
 
 class H(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Data_Analysis_Toolkit LIVE")
+        self.wfile.write(b"Data Analysis Toolkit LIVE")
 
-HTTPServer(("0.0.0.0",8080),H).serve_forever()
+HTTPServer(("0.0.0.0", PORT), H).serve_forever()
